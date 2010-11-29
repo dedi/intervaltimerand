1973a38c5ef249048b6f67353825dac196f4105e
dedi <devnull@localhost>
2010-11-29 08:08:12 +0200

    Added a new-BSD  copyright notice, as a preperation for publishing.

--- src/com/xomzom/androidstuff/timerapp/PausableTimer.java
+++ src/com/xomzom/androidstuff/timerapp/PausableTimer.java
@@ -1,3 +1,31 @@
+/**
+ * PeriodicTimer - a simple Interval Timer for Android
+ * Copyright (c) 2010, Dedi Hirschfeld
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the <organization> nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.xomzom.androidstuff.timerapp;
 
 import android.os.CountDownTimer;
@@ -10,8 +38,8 @@ import android.util.Log;
  * it will still tick 2 seconds when resumed.
  * Because I'm lazy, the counter will work with a specific interval timer
  * activity, and not using a listener. This can be changed if I ever need this
- * class for anything else. 
- * 
+ * class for anything else.
+ *
  * @author dedi
  */
 public class PausableTimer
@@ -19,7 +47,7 @@ public class PausableTimer
     //
     // Constants.
     //
-    
+
     /**
      * Milliseconds in a second.
      */
@@ -29,28 +57,28 @@ public class PausableTimer
     //
     // Members
     //
-    
+
     /**
      * Our listener object.
      */
     private TimerMainActivity m_listener;
-    
+
     /**
      * The actual countdown timer object. Will be null if the timer is currently
      * not running.
      */
     private CountDownTimer m_countdownTimer;
-    
+
     /**
      * Seconds remaining for this timer.
      */
     private int m_secondsRemaining;
-    
-    
+
+
     //
     // Operations.
     //
-    
+
     /**
      * Create a new pausable timer, associated with the given listener.
      */
@@ -58,41 +86,41 @@ public class PausableTimer
     {
         m_listener = listener;
     }
-    
+
     /**
      * Start a timer that will tick for the given number of seconds. This will
-     * cancel any currently running or paused timer. 
+     * cancel any currently running or paused timer.
      */
     public void start(int seconds)
     {
         if (m_countdownTimer != null)
         {
-            Log.d(this.getClass().toString(), 
+            Log.d(this.getClass().toString(),
                     "Warning: Timer started while running");
             m_countdownTimer.cancel();
             m_countdownTimer = null;
         }
         m_secondsRemaining = seconds;
-        m_countdownTimer = 
+        m_countdownTimer =
             new CountDownTimer(seconds * MILLIS_IN_SECOND, MILLIS_IN_SECOND) {
-            
+
             @Override
             public void onTick(long millisUntilFinished)
             {
-                int secondsRemaining = 
+                int secondsRemaining =
                     (int)(millisUntilFinished / MILLIS_IN_SECOND);
                 PausableTimer.this.onTick(secondsRemaining);
             }
-            
+
             @Override
             public void onFinish()
             {
-                PausableTimer.this.onFinish();                
+                PausableTimer.this.onFinish();
             }
         };
         m_countdownTimer.start();
     }
-    
+
     /**
      * Pause the timer.
      */
@@ -114,7 +142,7 @@ public class PausableTimer
         else
             onFinish();
     }
-    
+
     /**
      * Cancel a running timer.
      */
@@ -126,7 +154,7 @@ public class PausableTimer
 
     /**
      * A timer tick event.
-     * 
+     *
      * @param secondsRemaining
      */
     protected void onTick(int secondsRemaining)
--- src/com/xomzom/androidstuff/timerapp/SettingsActivity.java
+++ src/com/xomzom/androidstuff/timerapp/SettingsActivity.java
@@ -1,3 +1,31 @@
+/**
+ * PeriodicTimer - a simple Interval Timer for Android
+ * Copyright (c) 2010, Dedi Hirschfeld
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the <organization> nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.xomzom.androidstuff.timerapp;
 
 import android.os.Bundle;
@@ -20,15 +48,15 @@ public class SettingsActivity extends PreferenceActivity
     {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
-        
-        attachIntValidatingListener(R.string.pref_num_intervals_key, 1, 
+
+        attachIntValidatingListener(R.string.pref_num_intervals_key, 1,
                 Integer.MAX_VALUE); // Not likely, but still..
         attachIntValidatingListener(R.string.pref_interval_length_key, 1,
                 Integer.MAX_VALUE);
         attachIntValidatingListener(R.string.pref_countdown_key, 0,
                 Integer.MAX_VALUE);
     }
-    
+
     /**
      * Helper method: attach an onPreferenceChangeListener to the given
      * preference (identified by it's key string ID), to make sure that it's
@@ -57,14 +85,14 @@ public class SettingsActivity extends PreferenceActivity
                     }
                 });
     }
-    
+
     /**
      * Helper method: validate the new value of the given Preference,
      * and make sure that it is an integer between the given min and max values.
      * Also display an error message to the user if the value is invalid.
      *
      * @param preference The preference object.
-     * @param newValue The new preference value (which is presumed to be 
+     * @param newValue The new preference value (which is presumed to be
      * a string)
      * @param minAllowedValue The minimum allowed value.
      * @param maxAllowedValue The maximum allowed value.
@@ -85,7 +113,7 @@ public class SettingsActivity extends PreferenceActivity
             doValueError(minAllowedValue, maxAllowedValue);
             return false;
         }
-        
+
         if (valueAsInt < minAllowedValue || valueAsInt > maxAllowedValue)
         {
             doValueError(minAllowedValue, maxAllowedValue);
@@ -96,13 +124,13 @@ public class SettingsActivity extends PreferenceActivity
 
     /**
      * Helper method: Display a toast message saying that the preference value
-     * was not accepted, because it is not an integer in the given bounds. 
+     * was not accepted, because it is not an integer in the given bounds.
      * @param minAllowedValue
      * @param maxAllowedValue
      */
     private void doValueError(int minAllowedValue, int maxAllowedValue)
     {
-        String errMsg = getString(R.string.bad_int_pref_message, 
+        String errMsg = getString(R.string.bad_int_pref_message,
                 minAllowedValue, maxAllowedValue);
         Toast errToast = Toast.makeText(this, errMsg, Toast.LENGTH_LONG);
         errToast.show();
