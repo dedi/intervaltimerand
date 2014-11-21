cabe088e739b1e3c6d3da79eee31b737f2db7ba9
dedi <dedi@xomzom.com>
2014-11-21 14:03:29 +0200

    Small tweaks to make the project work with emacs cedet package

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-(
- (nil . ((compile-command-primary . "cd ~/work/inttimer && ant debug install -Dadb.device.arg=-e")
-         (compile-command-alternative . "cd ~/work/inttimer && ant debug install -Dadb.device.arg=-d")
-         (run-command-primary "cd ~/work/inttimer && ant debug install && adb shell am start -n com.xomzom.androidstuff.timerapp/com.xomzom.androidstuff.timerapp.TimerMainActivity")
-         (fill-column . 80)))
-)
